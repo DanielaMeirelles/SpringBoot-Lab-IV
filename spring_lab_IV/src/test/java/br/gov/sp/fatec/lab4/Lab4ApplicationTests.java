eef85226d203b7cde912f6a626f93a5b93354b7d
DanielaMeirelles <71828460+DanielaMeirelles@users.noreply.github.com>
2025-02-18 20:01:42 -0300

    Add files via upload

---
+++ spring_lab_IV/src/test/java/br/gov/sp/fatec/lab4/Lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
