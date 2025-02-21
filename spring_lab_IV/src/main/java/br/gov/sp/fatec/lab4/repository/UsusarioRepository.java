8dda008fadaf6001ec11db392173eabb06f239bb
DanielaMeirelles <71828460+DanielaMeirelles@users.noreply.github.com>
2025-02-21 00:03:50 +0000

    config DB connection

---
+++ spring_lab_IV/src/main/java/br/gov/sp/fatec/lab4/repository/UsusarioRepository.java
@@ -0,0 +1,9 @@
+package br.gov.sp.fatec.lab4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gov.sp.fatec.lab4.entity.Usuario;
+
+public interface UsusarioRepository extends JpaRepository<Usuario, Long>{
+
+}
