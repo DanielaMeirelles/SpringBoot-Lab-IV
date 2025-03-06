63fb851385f03c3bafc517743eb9ca9b5ff26cc1
DanielaMeirelles <71828460+DanielaMeirelles@users.noreply.github.com>
2025-03-06 23:16:54 +0000

    create class View

---
+++ src/main/java/br/gov/sp/fatec/lab4/entity/View.java
@@ -0,0 +1,12 @@
+package br.gov.sp.fatec.lab4.entity;
+
+public class View {
+    
+    public static class UsuarioSimplificado {};
+
+    public static class UsuarioCompleto {};
+
+    public static class AnotacaoSimplicada {};
+
+    
+}
